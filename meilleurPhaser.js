83afb01411ca0c59034121593e5c30ef3fc2c6d6
Elvis <59653433+Elvis-G@users.noreply.github.com>
2020-02-24 02:36:52 +0100

    40%

--- meilleurPhaser.js
+++ meilleurPhaser.js
@@ -1,8 +1,8 @@
 
 var config = {
 	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
+	width: 600,
+	height: 450,
 	physics: {
 		default: "arcade",
 		arcade: {
@@ -28,19 +28,28 @@ function init(){
 	var scoreText;
 	var bombes;
 	var pointDeVieText;
-
+	var ennemi;
 
 }
 var score = 0;
 var compteurSaut = 0;
 var pointDeVie = 3;
+var mouvEn = 0;
 
 function preload(){
 	this.load.image("background", "Assets/PixelAdventure1/Background/Test.png");
-	this.load.image("herbe", "Assets/PixelAdventure1/Terrain/Herbe.png");
+	this.load.image("support", "Assets/PixelAdventure1/Terrain/Support.png");
+	this.load.image("surface1", "Assets/PixelAdventure1/Terrain/Surface1.png");
+	this.load.image("surface1SB", "Assets/PixelAdventure1/Terrain/Surface1SB.png");
+	this.load.image("surface1BD", "Assets/PixelAdventure1/Terrain/Surface1BD.png");
+	this.load.image("surface1BG", "Assets/PixelAdventure1/Terrain/Surface1BG.png");
 	this.load.image("bombes", "bombes.png");
+
 	this.load.spritesheet("collectibles", "Assets/PixelAdventure1/Items/Fruits/Bananas.png",
 	{frameWidth: 32, frameHeight:32});
+	this.load.spritesheet("recup", "Assets/PixelAdventure1/Items/Fruits/collected.png",
+	{frameWidth: 32, frameHeight:32});
+	// Animations Perso joueur
 	this.load.spritesheet("perso", "Assets/PixelAdventure1/MainCharacters/NinjaFrog/Idle.png",
 	{frameWidth: 32, frameHeight:32});
 	this.load.spritesheet("persorun", "Assets/PixelAdventure1/MainCharacters/NinjaFrog/Run.png",
@@ -49,37 +58,56 @@ function preload(){
 	{frameWidth: 32, frameHeight:32});
 	this.load.spritesheet("persodoublejump", "Assets/PixelAdventure1/MainCharacters/NinjaFrog/DoubleJump.png",
 	{frameWidth: 32, frameHeight:32});
-	this.load.spritesheet("recup", "Assets/PixelAdventure1/Items/Fruits/collected.png",
+	this.load.spritesheet("persohit", "Assets/PixelAdventure1/MainCharacters/NinjaFrog/Hit.png",
 	{frameWidth: 32, frameHeight:32});
-
+	// Animations Ennemi
+	this.load.spritesheet("ennemi", "Assets/PixelAdventure1/Enemies/Trunk/Idle.png",
+	{frameWidth: 64, frameHeight:32});
+	this.load.spritesheet("ennemirun", "Assets/PixelAdventure1/Enemies/Trunk/Run.png",
+	{frameWidth: 64, frameHeight:32});
+	this.load.spritesheet("ennemiattack", "Assets/PixelAdventure1/Enemies/Trunk/Attack.png",
+	{frameWidth: 64, frameHeight:32});
 }
 
 function create(){
 	this.add.image(400,300,"background");
 
 	platforms = this.physics.add.staticGroup ();
-	platforms.create(790,615,"herbe").setScale(1).refreshBody();
-	platforms.create(745,615,"herbe").setScale(1).refreshBody();
-	platforms.create(700,615,"herbe").setScale(1).refreshBody();
-	platforms.create(655,615,"herbe").setScale(1).refreshBody();
-	platforms.create(610,615,"herbe").setScale(1).refreshBody();
-	platforms.create(565,615,"herbe").setScale(1).refreshBody();
-	platforms.create(520,615,"herbe").setScale(1).refreshBody();
-	platforms.create(475,615,"herbe").setScale(1).refreshBody();
-	platforms.create(430,615,"herbe").setScale(1).refreshBody();
-	platforms.create(385,615,"herbe").setScale(1).refreshBody();
-	platforms.create(340,615,"herbe").setScale(1).refreshBody();
-	platforms.create(295,615,"herbe").setScale(1).refreshBody();
-	platforms.create(250,615,"herbe").setScale(1).refreshBody();
-	platforms.create(205,615,"herbe").setScale(1).refreshBody();
-	platforms.create(160,615,"herbe").setScale(1).refreshBody();
-	platforms.create(115,615,"herbe").setScale(1).refreshBody();
-	platforms.create(70,615,"herbe").setScale(1).refreshBody();
-	platforms.create(35,615,"herbe").setScale(1).refreshBody();
-	platforms.create(-10,615,"herbe").setScale(1).refreshBody();
+// Le bas de l'écran
+	platforms.create(591,445,"support").setScale(1).refreshBody();
+	platforms.create(567,445,"support").setScale(1).refreshBody();
+	platforms.create(543,445,"support").setScale(1).refreshBody();
+	platforms.create(519,445,"support").setScale(1).refreshBody();
+	platforms.create(495,445,"support").setScale(1).refreshBody();
+	platforms.create(471,445,"support").setScale(1).refreshBody();
+	platforms.create(447,445,"support").setScale(1).refreshBody();
+	platforms.create(423,445,"support").setScale(1).refreshBody();
+	platforms.create(399,445,"support").setScale(1).refreshBody();
+	platforms.create(375,445,"support").setScale(1).refreshBody();
+	platforms.create(351,445,"support").setScale(1).refreshBody();
+	platforms.create(327,445,"support").setScale(1).refreshBody();
+	platforms.create(303,445,"support").setScale(1).refreshBody();
+	platforms.create(279,445,"support").setScale(1).refreshBody();
+	platforms.create(255,445,"support").setScale(1).refreshBody();
+	platforms.create(231,445,"support").setScale(1).refreshBody();
+	platforms.create(207,445,"support").setScale(1).refreshBody();
+	platforms.create(183,445,"support").setScale(1).refreshBody();
+	platforms.create(159,445,"support").setScale(1).refreshBody();
+	platforms.create(135,445,"support").setScale(1).refreshBody();
+	platforms.create(111,445,"support").setScale(1).refreshBody();
+	platforms.create(87,445,"support").setScale(1).refreshBody();
+	platforms.create(63,445,"support").setScale(1).refreshBody();
+	platforms.create(39,445,"support").setScale(1).refreshBody();
+	platforms.create(15,445,"support").setScale(1).refreshBody();
+// Les plateformes
+/*	platforms.create(114,350,"surface1SB").setScale(1).refreshBody();
+	platforms.create(69,350,"surface1SB").setScale(1).refreshBody();
+	platforms.create(23,350,"surface1BG").setScale(1).refreshBody();
+	platforms.create(577,150,"surface1BD").setScale(1).refreshBody(); */
 
-	player = this.physics.add.sprite(100,450,"perso");
 
+// LE JOUEUR
+	player = this.physics.add.sprite(100,350,"perso");
 	player.setCollideWorldBounds(true);
 	this.physics.add.collider(player,platforms);
 	player.setBounce(0);
@@ -87,8 +115,17 @@ function create(){
 
 	cursors = this.input.keyboard.createCursorKeys();
 
+// L'ENNEMI
+ennemi = this.physics.add.sprite(400,350,"ennemi");
+ennemi.setCollideWorldBounds(true);
+this.physics.add.collider(ennemi, platforms);
+ennemi.setBounce(0);
+ennemi.body.setGravityY(300);
 
-// DÉPLACEMENTS
+
+
+
+// DÉPLACEMENTS JOUEUR
 	this.anims.create({
 		key: "gauche",
 		frames: this.anims.generateFrameNumbers("persorun", {start: 0, end: 11}),
@@ -112,6 +149,33 @@ function create(){
 	frameRate: 20,
 	repeat:-1
 	});
+	this.anims.create({
+	key: "degatsPris",
+	frames: this.anims.generateFrameNumbers("persohit", {start: 0, end: 6}),
+	frameRate: 20,
+	repeat:-1
+	});
+
+
+// DÉPLACEMENTS ENNEMI
+this.anims.create({
+key: "runEnnemi",
+frames: this.anims.generateFrameNumbers("ennemirun", {start: 0, end: 13}),
+frameRate: 20,
+repeat:-1
+});
+this.anims.create({
+key: "attackEnnemi",
+frames: this.anims.generateFrameNumbers("ennemiattack", {start: 0, end: 10}),
+frameRate: 20,
+repeat:-1
+});
+this.anims.create({
+key: "baseEnnemi",
+frames: this.anims.generateFrameNumbers("ennemi", {start: 0, end: 17}),
+frameRate: 20,
+repeat:-1
+});
 
 
 // OBJETS
@@ -130,7 +194,7 @@ repeat:-1
 
 collectibles = this.physics.add.group({
 key: "collectibles",
-repeat: 11,
+repeat: 8,
 setXY: {x: 12, y: 0, stepX: 70}
 });
 
@@ -138,13 +202,14 @@ setXY: {x: 12, y: 0, stepX: 70}
 	bombes = this.physics.add.group();
 
 	this.physics.add.collider(bombes, platforms);
+	this.physics.add.overlap(player, ennemi);
 	this.physics.add.collider(player, bombes, hitbomb, null, this);
 
 	this.physics.add.collider(collectibles, platforms);
 	this.physics.add.overlap(collectibles, player, collectCollectible, null, this);
 
-	scoreText = this.add.text(10, 10, "score : 0", {fontSize: "24px", fill:'#fff'});
-	pointDeVieText = this.add.text(700, 10, "PV : 3", {fontSize: "24px", fill:'#fff'});
+	scoreText = this.add.text(10, 10, "score : 0", {fontSize: "16px", fill:'#fff'});
+	pointDeVieText = this.add.text(530, 10, "PV : 3", {fontSize: "16px", fill:'#fff'});
 }
 
 function hitbomb (player, bombe) {
@@ -175,6 +240,26 @@ function collectCollectible(player, collectible) {
 
 function update(){
 
+
+/* Lorsque l'ennemi rentre en collision avec une surface change de direction
+setVeolictyX a lui attribuer pour qu'il se déplace avec l'animation lié (avec setFlipX comme pour le joueur ) => le tout similaire au joueur*/
+
+/* Lorsque l'ennemi nous voie, le faire attaquer
+l'attaque génère un pépin qui se déplace dans la direction où il regarde
+si le pépin entre en contact avec le joueur -1 pv */
+
+/* si le joueur saute sur l'ennemi et que son hauteur est supérieur à celui de l'ennemi, alors au moment de collide il le tue
+à l'inverse il perd un point de vie s'il rentre en contact avec celui-ci*/
+
+/* SI il y a le temps, placé moi-même les bananes, (voir d'autres fruits si plus de temps) et faire un décors joli */
+
+/* Lorsque tout les fruits sont récupérés gagnent une vie */
+
+/* Demander à Joseph :
+ - Comment faire pour que l'animation se déroule entièrement
+ - Quelle commande rentrer pour que l'ennemi est la capacité d'attaquer lorsqu'il est face à face avec nous
+ - Comment faire pour prendre la hauteur de l'ennemi et la notre */
+
 	if(cursors.left.isDown){
 		player.setVelocityX(-160);
 		player.anims.play("gauche", true);
@@ -187,7 +272,6 @@ function update(){
 		player.setVelocityX(0);
 		player.anims.play("base", true);
 	}
-
 
 	// PREMIER jump
 	if (cursors.up.isDown && player.body.touching.down && compteurSaut == 0){
@@ -218,6 +302,8 @@ function update(){
 	if (compteurSaut >= 3) {
 		compteurSaut = 0;
 	}
+
+
 
 
 
