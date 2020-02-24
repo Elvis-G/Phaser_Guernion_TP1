c7de33351844afaa07871f5728537c031a31f45d
Elvis <59653433+Elvis-G@users.noreply.github.com>
2020-02-24 03:12:06 +0100

    Alpha 1.0.0.1

--- meilleurPhaser.js
+++ meilleurPhaser.js
@@ -21,14 +21,16 @@ var config = {
 var game = new Phaser.Game(config);
 
 function init(){
+	var cursors;
+
 	var platforms;
 	var player;
-	var cursors;
-	var collectibles;
-	var scoreText;
-	var bombes;
-	var pointDeVieText;
 	var ennemi;
+	var bananes;
+
+	var scoreText;
+	var pointDeVieText;
+	var bombes;
 
 }
 var score = 0;
@@ -39,18 +41,15 @@ var mouvEn = 0;
 function preload(){
 	this.load.image("background", "Assets/PixelAdventure1/Background/Test.png");
 	this.load.image("support", "Assets/PixelAdventure1/Terrain/Support.png");
-	this.load.image("surface1", "Assets/PixelAdventure1/Terrain/Surface1.png");
-	this.load.image("surface1SB", "Assets/PixelAdventure1/Terrain/Surface1SB.png");
-	this.load.image("surface1BD", "Assets/PixelAdventure1/Terrain/Surface1BD.png");
-	this.load.image("surface1BG", "Assets/PixelAdventure1/Terrain/Surface1BG.png");
 	this.load.image("bombes", "bombes.png");
 
-	this.load.spritesheet("collectibles", "Assets/PixelAdventure1/Items/Fruits/Bananas.png",
+	// Animations Collectibles
+	this.load.spritesheet("bananeidle", "Assets/PixelAdventure1/Items/Fruits/Bananas.png",
 	{frameWidth: 32, frameHeight:32});
-	this.load.spritesheet("recup", "Assets/PixelAdventure1/Items/Fruits/collected.png",
+	this.load.spritesheet("bananerecup", "Assets/PixelAdventure1/Items/Fruits/Collected.png",
 	{frameWidth: 32, frameHeight:32});
 	// Animations Perso joueur
-	this.load.spritesheet("perso", "Assets/PixelAdventure1/MainCharacters/NinjaFrog/Idle.png",
+	this.load.spritesheet("persoidle", "Assets/PixelAdventure1/MainCharacters/NinjaFrog/Idle.png",
 	{frameWidth: 32, frameHeight:32});
 	this.load.spritesheet("persorun", "Assets/PixelAdventure1/MainCharacters/NinjaFrog/Run.png",
 	{frameWidth: 32, frameHeight:32});
@@ -61,7 +60,7 @@ function preload(){
 	this.load.spritesheet("persohit", "Assets/PixelAdventure1/MainCharacters/NinjaFrog/Hit.png",
 	{frameWidth: 32, frameHeight:32});
 	// Animations Ennemi
-	this.load.spritesheet("ennemi", "Assets/PixelAdventure1/Enemies/Trunk/Idle.png",
+	this.load.spritesheet("ennemiidle", "Assets/PixelAdventure1/Enemies/Trunk/Idle.png",
 	{frameWidth: 64, frameHeight:32});
 	this.load.spritesheet("ennemirun", "Assets/PixelAdventure1/Enemies/Trunk/Run.png",
 	{frameWidth: 64, frameHeight:32});
@@ -100,61 +99,61 @@ function create(){
 	platforms.create(39,445,"support").setScale(1).refreshBody();
 	platforms.create(15,445,"support").setScale(1).refreshBody();
 // Les plateformes
-/*	platforms.create(114,350,"surface1SB").setScale(1).refreshBody();
-	platforms.create(69,350,"surface1SB").setScale(1).refreshBody();
-	platforms.create(23,350,"surface1BG").setScale(1).refreshBody();
-	platforms.create(577,150,"surface1BD").setScale(1).refreshBody(); */
 
 
 // LE JOUEUR
-	player = this.physics.add.sprite(100,350,"perso");
-	player.setCollideWorldBounds(true);
-	this.physics.add.collider(player,platforms);
-	player.setBounce(0);
-	player.body.setGravityY(300);
+player = this.physics.add.sprite(100,350,"persoidle");
+player.setCollideWorldBounds(true);
+this.physics.add.collider(player,platforms);
+player.setBounce(0);
+player.body.setGravityY(300);
 
-	cursors = this.input.keyboard.createCursorKeys();
+cursors = this.input.keyboard.createCursorKeys();
 
 // L'ENNEMI
-ennemi = this.physics.add.sprite(400,350,"ennemi");
+ennemi = this.physics.add.sprite(400,350,"ennemiidle");
 ennemi.setCollideWorldBounds(true);
 this.physics.add.collider(ennemi, platforms);
 ennemi.setBounce(0);
 ennemi.body.setGravityY(300);
 
-
+// LES COLLECTIBLES
+/* bananes = this.physics.add.sprite(50,400,"bananeidle");
+this.physics.add.collider(bananes,platforms);
+bananes.setBounce(0);
+bananes.body.setGravityY(0); */
 
 
 // DÉPLACEMENTS JOUEUR
-	this.anims.create({
-		key: "gauche",
-		frames: this.anims.generateFrameNumbers("persorun", {start: 0, end: 11}),
-		frameRate: 20,
-		repeat:-1
-	});
-	this.anims.create({
-		key: "saut",
-		frames: [{key: "persojump", frame: 0}],
-		frameRate: 20,
-	});
-	this.anims.create({
-	key: "doubleSaut",
-	frames: this.anims.generateFrameNumbers("persodoublejump", {start: 0, end: 5}),
+this.anims.create({
+	key: "gauche",
+	frames: this.anims.generateFrameNumbers("persorun", {start: 0, end: 11}),
 	frameRate: 20,
 	repeat:-1
-	});
-	this.anims.create({
-	key: "base",
-	frames: this.anims.generateFrameNumbers("perso", {start: 0, end: 10}),
+});
+this.anims.create({
+	key: "saut",
+	frames: [{key: "persojump", frame: 0}],
 	frameRate: 20,
-	repeat:-1
-	});
-	this.anims.create({
-	key: "degatsPris",
-	frames: this.anims.generateFrameNumbers("persohit", {start: 0, end: 6}),
-	frameRate: 20,
-	repeat:-1
-	});
+});
+this.anims.create({
+key: "doubleSaut",
+frames: this.anims.generateFrameNumbers("persodoublejump", {start: 0, end: 5}),
+frameRate: 20,
+repeat:-1
+});
+this.anims.create({
+key: "base",
+frames: this.anims.generateFrameNumbers("persoidle", {start: 0, end: 10}),
+frameRate: 20,
+repeat:-1
+});
+this.anims.create({
+key: "degatsPris",
+frames: this.anims.generateFrameNumbers("persohit", {start: 0, end: 6}),
+frameRate: 20,
+repeat:-1
+});
 
 
 // DÉPLACEMENTS ENNEMI
@@ -172,41 +171,41 @@ repeat:-1
 });
 this.anims.create({
 key: "baseEnnemi",
-frames: this.anims.generateFrameNumbers("ennemi", {start: 0, end: 17}),
+frames: this.anims.generateFrameNumbers("ennemiidle", {start: 0, end: 17}),
 frameRate: 20,
 repeat:-1
 });
 
 
-// OBJETS
+// COLLECTIBLES
 this.anims.create({
-key: "bananeAnimation",
-frames: this.anims.generateFrameNumbers("banane", {start: 0, end: 16}),
+key: "bananeAnim",
+frames: this.anims.generateFrameNumbers("bananeidle", {start: 0, end: 16}),
 frameRate: 20,
 repeat:-1
 });
 this.anims.create({
-key: "collecteAnimation",
-frames: this.anims.generateFrameNumbers("recup", {start: 0, end: 5}),
+key: "collecteBananeAnim",
+frames: this.anims.generateFrameNumbers("bananerecup", {start: 0, end: 5}),
 frameRate: 20,
 repeat:-1
 });
 
-collectibles = this.physics.add.group({
-key: "collectibles",
+bananes = this.physics.add.group({
+key: "bananeidle",
 repeat: 8,
 setXY: {x: 12, y: 0, stepX: 70}
 });
 
 
-	bombes = this.physics.add.group();
-
-	this.physics.add.collider(bombes, platforms);
 	this.physics.add.overlap(player, ennemi);
+
+	bombes = this.physics.add.group();
+	this.physics.add.collider(bombes, platforms);
 	this.physics.add.collider(player, bombes, hitbomb, null, this);
 
-	this.physics.add.collider(collectibles, platforms);
-	this.physics.add.overlap(collectibles, player, collectCollectible, null, this);
+	this.physics.add.collider(bananes, platforms);
+	this.physics.add.overlap(bananes, player, collectCollectible, null, this);
 
 	scoreText = this.add.text(10, 10, "score : 0", {fontSize: "16px", fill:'#fff'});
 	pointDeVieText = this.add.text(530, 10, "PV : 3", {fontSize: "16px", fill:'#fff'});
@@ -222,13 +221,13 @@ function hitbomb (player, bombe) {
 	}
 }
 
-function collectCollectible(player, collectible) {
-	collectible.disableBody(true,true)
+function collectCollectible(player, banane) {
+	banane.disableBody(true,true)
 	score+= 10;
 	scoreText.setText("score : "+score);
-	if (collectibles.countActive(true)=== 0) {
-		collectibles.children.iterate(function(child){
-			child.enableBody(true, child.x, 0, true, true);
+	if (bananes.countActive(true)=== 0) {
+		bananes.children.iterate(function(child){
+		  child.enableBody(true, child.x, 0, true, true);
 		});
 		var x = (player.x<400)? Phaser.Math.Between(400,800) : Phaser.Math.Between(0,400);
 		var bombe = bombes.create(x, 16, "bombes");
@@ -239,6 +238,8 @@ function collectCollectible(player, collectible) {
 }
 
 function update(){
+
+
 
 
 /* Lorsque l'ennemi rentre en collision avec une surface change de direction
@@ -302,10 +303,5 @@ si le pépin entre en contact avec le joueur -1 pv */
 	if (compteurSaut >= 3) {
 		compteurSaut = 0;
 	}
-
-
-
-
-
 
 }
